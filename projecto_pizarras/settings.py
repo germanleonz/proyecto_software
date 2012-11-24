6aa09c5b5371f1cac99e0f0ab610020c2e544920
= <=>
2012-11-24 16:02:33 -0430

    pulliiiinngggg

--- projecto_pizarras/settings.py
+++ projecto_pizarras/settings.py
@@ -60,6 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
+
 STATIC_ROOT = '/home/juan/Software/proyecto_pizarras/static'
 
 # URL prefix for static files.
@@ -122,8 +123,8 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 
+	'/home/carla/ProyectoSofware/proyecto_software/templates'
 
-    '/home/juan/Software/proyecto_pizarras/templates'
 
 )
 
