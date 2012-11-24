eb8cd0417199403918a54140ca67c3cb0fcb828c
germanleonz <germanleonz@gmail.com>
2012-11-24 12:53:15 -0430

    Cambiando el readme

--- projecto_pizarras/settings.py
+++ projecto_pizarras/settings.py
@@ -12,8 +12,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'projecto_pizarra',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
+        'USER': 'software',                      # Not used with sqlite3.
+        'PASSWORD': 'software',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -121,9 +121,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-
-
-    '/home/juan/Software/proyecto_pizarras/templates'
+    '/home/germanleonz/Python/Projects/proyecto_software/templates',
 
 )
 
